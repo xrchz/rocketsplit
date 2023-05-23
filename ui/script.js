54e3f8cf274f98f866e8f5eac1319eca22398b38
Ramana Kumar <ramana@member.fsf.org>
2023-05-23 08:14:35 +0100

    Rework to do everything client-side, use browser provider

--- ui/script.js
+++ ui/script.js
@@ -1,4 +1,15 @@
-const socket = io()
-import { ethers } from "./node_modules/ethers/dist/ethers.esm.min.js"
+import { ethers } from "./node_modules/ethers/dist/ethers.js"
 
 const fragment = document.createDocumentFragment()
+
+const provider = new ethers.BrowserProvider(window.ethereum)
+const network = await provider.getNetwork()
+console.log(`Connected to ${network.name}`)
+console.log(`Available accounts: ${await provider.listAccounts().then((a) => Promise.all(a.map((s) => s.getAddress()))).then((a) => JSON.stringify(a))}`)
+
+/*
+const abi = await fetch('RocketSplit.json').then((res) => res.json()).abi
+
+const factory = new ethers.Contract(process.env.ADDRESS, abi, provider)
+console.log(`Factory contract is ${await factory.getAddress()}`)
+*/

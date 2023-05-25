8a971e49d4dbc882dec0b534334679d81f9c1ad5
Ramana Kumar <ramana@member.fsf.org>
2023-05-25 22:12:06 +0100

    Add more ui

--- ui/script.js
+++ ui/script.js
@@ -5,10 +5,30 @@ const fragment = document.createDocumentFragment()
 const provider = new ethers.BrowserProvider(window.ethereum)
 const network = await provider.getNetwork()
 console.log(`Connected to ${network.name}`)
-console.log(`Available accounts: ${await provider.listAccounts().then((a) => Promise.all(a.map((s) => s.getAddress()))).then((a) => JSON.stringify(a))}`)
 const abi = await fetch('RocketSplit.json').then(res => res.json()).then(j => j.abi)
 const factory = new ethers.Contract(await fetch('RocketSplitAddress.json').then(res => res.json()), abi, provider)
 console.log(`Factory contract is ${await factory.getAddress()}`)
+let signer
+
+const walletSection = document.createElement('section')
+const signerLabel = walletSection.appendChild(document.createElement('label'))
+signerLabel.classList.add('address')
+signerLabel.innerText = 'Connected account: '
+const signerInput = signerLabel.appendChild(document.createElement('input'))
+signerInput.type = 'text'
+signerInput.setAttribute('readonly', true)
+signerInput.placeholder = 'none'
+
+async function signerConnected() {
+  signerInput.value = ''
+  signer = await provider.getSigner()
+  if (signer)
+    signerInput.value = await signer.getAddress()
+}
+
+window.ethereum.on('connect', signerConnected)
+window.ethereum.on('accountsChanged', signerConnected)
+await signerConnected()
 
 const addressPattern = '^(?:0x[0-9a-fA-F]{40})|(?:.{3,}\.eth)$'
 
@@ -20,6 +40,21 @@ description.innerText = 'Information about what this form does should go here. N
 const createInputsDiv = createSection.appendChild(document.createElement('div'))
 createInputsDiv.classList.add('inputs')
 
+const button = document.createElement('input')
+button.type = 'button'
+button.value = 'Deploy Contract'
+button.disabled = true
+
+function canSubmit() {
+  const addresses = Array.from(
+    createInputsDiv.querySelectorAll('.address > input')
+  ).every(a => a.value && a.checkValidity())
+  const fees = Array.from(
+    createInputsDiv.querySelectorAll('.feefraction')
+  ).every(s => s.innerText)
+  return addresses && fees
+}
+
 function addInputs(asset) {
   const div = createInputsDiv.appendChild(document.createElement('div'))
   const addressLabel = div.appendChild(document.createElement('label'))
@@ -30,6 +65,21 @@ function addInputs(asset) {
   addressInput.pattern = addressPattern
   const ensName = addressLabel.appendChild(document.createElement('span'))
   ensName.classList.add('ens')
+  const feeNLabel = div.appendChild(document.createElement('label'))
+  feeNLabel.innerText = `${asset} Fee Numerator`
+  feeNLabel.classList.add('fee')
+  const feeNInput = feeNLabel.appendChild(document.createElement('input'))
+  feeNInput.type = 'number'
+  feeNInput.min = 0
+  const feeDLabel = div.appendChild(document.createElement('label'))
+  feeDLabel.innerText = `${asset} Fee Denominator`
+  feeDLabel.classList.add('fee')
+  const feeDInput = feeDLabel.appendChild(document.createElement('input'))
+  feeDInput.type = 'number'
+  feeDInput.min = 1
+  const feeFraction = div.appendChild(document.createElement('span'))
+  feeFraction.innerText = ''
+  feeFraction.classList.add('fraction')
   addressInput.addEventListener('change', async () => {
     addressInput.setCustomValidity('')
     ensName.innerText = ''
@@ -51,24 +101,11 @@ function addInputs(asset) {
       }
     }
     addressInput.reportValidity()
+    button.disabled = !canSubmit()
   })
-  const feeNLabel = div.appendChild(document.createElement('label'))
-  feeNLabel.innerText = `${asset} Fee Numerator`
-  feeNLabel.classList.add('fee')
-  const feeNInput = feeNLabel.appendChild(document.createElement('input'))
-  feeNInput.type = 'number'
-  feeNInput.min = 0
-  const feeDLabel = div.appendChild(document.createElement('label'))
-  feeDLabel.innerText = `${asset} Fee Denominator`
-  feeDLabel.classList.add('fee')
-  const feeDInput = feeDLabel.appendChild(document.createElement('input'))
-  feeDInput.type = 'number'
-  feeDInput.min = 1
-  const feeFraction = div.appendChild(document.createElement('span'))
-  feeFraction.innerText = '0'
   function updateFees() {
     feeNInput.max = feeDInput.value
-    feeFraction.innerText = '0'
+    feeFraction.innerText = ''
     if (feeNInput.checkValidity() && feeDInput.checkValidity()) {
       if (!feeNInput.value) feeNInput.value = 0
       if (!feeDInput.value) feeDInput.value = feeNInput.value || 1
@@ -78,20 +115,24 @@ function addInputs(asset) {
     }
     feeDInput.reportValidity()
     feeNInput.reportValidity()
+    button.disabled = !canSubmit()
   }
   feeNInput.addEventListener('change', updateFees)
   feeDInput.addEventListener('change', updateFees)
 }
 
+// TODO: add node address. probably also check it is a registered node.
 addInputs('ETH')
 addInputs('RPL')
-const button = createInputsDiv.appendChild(document.createElement('input'))
-button.type = 'button'
-button.value = 'Deploy Contract'
+createInputsDiv.appendChild(button)
+button.addEventListener('click', async () => {
+  // TODO: await factory.connect(signer).deploy()
+})
 
 const changeSection = document.createElement('section')
 changeSection.appendChild(document.createElement('h2')).innerText = 'View/Use Marriage Contract'
 
 const body = document.querySelector('body')
+body.appendChild(walletSection)
 body.appendChild(createSection)
 body.appendChild(changeSection)

a51a67154757649891deeb368aa85ce2aff2be09
Ramana Kumar <ramana@member.fsf.org>
2023-11-30 16:12:20 +0000

    Get the unclaimed intervals

--- react-ui/src/components/ClaimIntervals.js
+++ react-ui/src/components/ClaimIntervals.js
@@ -5,38 +5,36 @@ const ClaimIntervals = ({ nodeAddress, setPendingClaims, setNodeMinipools }) => 
 
     const [rocketRewardsPoolAddress, setRocketRewardsPoolAddress] = useState(null);
     const [rocketMinipoolManagerAddress, setRocketMinipoolManagerAddress] = useState(null);
+    const [rocketMerkleDistributorAddress, setRocketMerkleDistributorAddress] = useState(null);
     const [currentIntervalIndex, setCurrentIntervalIndex] = useState(0);
-    const [validatingMinipoolCount, setValidatingMinipoolCount] = useState(0);
+    const [minipoolCount, setMinipoolCount] = useState(0);
+    const [unclaimedIntervals, setUnclaimedIntervals] = useState(null);
 
     const { chain } = useNetwork();
-    const minipoolManagerAbi = [{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"}],"name":"getNodeActiveMinipoolCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"},{"internalType":"uint256","name":"_index","type":"uint256"}],"name":"getNodeMinipoolAt","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"}],"name":"getNodeMinipoolCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"}],"name":"getNodeStakingMinipoolCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"},{"internalType":"uint256","name":"_index","type":"uint256"}],"name":"getNodeValidatingMinipoolAt","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"}],"name":"getNodeValidatingMinipoolCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_offset","type":"uint256"},{"internalType":"uint256","name":"_limit","type":"uint256"}],"name":"getPrelaunchMinipools","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getStakingMinipoolCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_index","type":"uint256"}],"name":"getVacantMinipoolAt","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVacantMinipoolCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}]
+    const minipoolManagerAbi = [{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"}],"name":"getNodeActiveMinipoolCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"},{"internalType":"uint256","name":"_index","type":"uint256"}],"name":"getNodeMinipoolAt","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"}],"name":"getNodeMinipoolCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"}],"name":"getNodeStakingMinipoolCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"},{"internalType":"uint256","name":"_index","type":"uint256"}],"name":"getNodeValidatingMinipoolAt","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"}],"name":"getNodeValidatingMinipoolCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_offset","type":"uint256"},{"internalType":"uint256","name":"_limit","type":"uint256"}],"name":"getPrelaunchMinipools","outputs":[{"internalType":"address[]","name":"","type":"address[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getStakingMinipoolCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"_index","type":"uint256"}],"name":"getVacantMinipoolAt","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getVacantMinipoolCount","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"}];
+    const merkleDistributorAbi = [{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"},{"internalType":"uint256[]","name":"_rewardIndex","type":"uint256[]"},{"internalType":"uint256[]","name":"_amountRPL","type":"uint256[]"},{"internalType":"uint256[]","name":"_amountETH","type":"uint256[]"},{"internalType":"bytes32[][]","name":"_merkleProof","type":"bytes32[][]"}],"name":"claim","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_nodeAddress","type":"address"},{"internalType":"uint256[]","name":"_rewardIndex","type":"uint256[]"},{"internalType":"uint256[]","name":"_amountRPL","type":"uint256[]"},{"internalType":"uint256[]","name":"_amountETH","type":"uint256[]"},{"internalType":"bytes32[][]","name":"_merkleProof","type":"bytes32[][]"},{"internalType":"uint256","name":"_stakeAmount","type":"uint256"}],"name":"claimAndStake","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_rewardIndex","type":"uint256"},{"internalType":"address","name":"_claimer","type":"address"}],"name":"isClaimed","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"view","type":"function"}];
 
     const storageContractConfig = {
         address: chain?.id === 17000 ? process.env.REACT_APP_ROCKETPOOL_STORAGE_ADDRESS_HOLESKY : process.env.REACT_APP_ROCKETPOOL_STORAGE_ADDRESS_MAINNET,
         abi: [{"inputs":[{"internalType":"bytes32","name":"_key","type":"bytes32"}],"name":"getAddress","outputs":[{"internalType":"address","name":"r","type":"address"}],"stateMutability":"view","type":"function"}]
     };
 
-    useContractRead({
-        ...storageContractConfig,
-        functionName: "getAddress",
-        args: [keccak256("contract.addressrocketRewardsPool")],
-        onLoading: () => console.log("Loading..."),
-        onError: (error) => console.log("Error: " + error),
-        onSuccess: (result) => {
-            setRocketRewardsPoolAddress(result);
-        }
-    })
+    const getAddress = (name, setAddress) => {
+      useContractRead({
+          ...storageContractConfig,
+          functionName: "getAddress",
+          args: [keccak256(`contract.address${name}`)],
+          onLoading: () => console.log("Loading..."),
+          onError: (error) => console.log("Error: " + error),
+          onSuccess: (result) => {
+              setAddress(result);
+          }
+      })
+    }
 
-    useContractRead({
-        ...storageContractConfig,
-        functionName: "getAddress",
-        args: [keccak256("contract.addressrocketMinipoolManager")],
-        onLoading: () => console.log("Loading..."),
-        onError: (error) => console.log("Error: " + error),
-        onSuccess: (result) => {
-            setRocketMinipoolManagerAddress(result);
-        }
-    })
+    getAddress('rocketRewardsPool', setRocketRewardsPoolAddress)
+    getAddress('rocketMinipoolManager', setRocketMinipoolManagerAddress)
+    getAddress('rocketMerkleDistributor', setRocketMerkleDistributorAddress)
 
     useContractRead({
         address: rocketRewardsPoolAddress,
@@ -46,6 +44,7 @@ const ClaimIntervals = ({ nodeAddress, setPendingClaims, setNodeMinipools }) => 
         args: [],
         onError: (error) => console.log("Error: " + error),
         onSuccess: (result) => {
+            console.log(`current reward interval index ${result}`) // TODO: for debug only
             setCurrentIntervalIndex(result);
         }
     })
@@ -54,22 +53,37 @@ const ClaimIntervals = ({ nodeAddress, setPendingClaims, setNodeMinipools }) => 
         address: rocketMinipoolManagerAddress,
         enabled: nodeAddress && rocketMinipoolManagerAddress,
         abi: minipoolManagerAbi,
-        functionName: "getNodeValidatingMinipoolCount",
+        functionName: "getNodeMinipoolCount",
         args: [nodeAddress],
         onError: (error) => console.log("Error: " + error),
         onSuccess: (result) => {
-            setValidatingMinipoolCount(result);
+            console.log(`${nodeAddress} minipool count ${result}`) // TODO: for debug only
+            setMinipoolCount(result);
         }
     })
 
     useContractReads({
-        enabled: nodeAddress && rocketMinipoolManagerAddress && validatingMinipoolCount,
-        contracts: Array.from(Array(validatingMinipoolCount).keys()).map(i =>
-          ({address: rocketMinipoolManagerAddress,
-            abi: minipoolManagerAbi,
-            functionName: "getNodeValidatingMinipoolAt",
-            args: [nodeAddress, i]})),
+        enabled: nodeAddress && rocketMinipoolManagerAddress && minipoolCount,
+        contracts: Array.from(Array(minipoolCount).keys()).map(i =>
+            ({address: rocketMinipoolManagerAddress,
+              abi: minipoolManagerAbi,
+              functionName: "getNodeMinipoolAt",
+              args: [nodeAddress, i]})),
         onSuccess: (data) => setNodeMinipools(data)
+    })
+
+    useContractReads({
+        enabled: nodeAddress && rocketMerkleDistributorAddress && currentIntervalIndex,
+        contracts: Array.from(Array(currentIntervalIndex).keys()).map(i =>
+            ({address: rocketMerkleDistributorAddress,
+              abi: merkleDistributorAbi,
+              functionName: "isClaimed"
+              args: [currentIntervalIndex, nodeAddress]})),
+        onSuccess: (data) => {
+            const result = data.flatMap((claimed, index) => claimed ? [] : [index])
+            console.log(`${nodeAddress} got ${result.length} unclaimed intervals: ${result}`) // TODO: for debug only
+            setUnclaimedIntervals(result)
+        }
     })
 
 }
